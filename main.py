476060d1ae7cf0969d2d4d33371280eedea6ddba
Tyler <dialupdreams@proton.me>
2025-07-27 18:53:16 -0400

    Add logic to main.py - Using BeautifulSoup since we're using HTML! - imported bs4 and wrote the logic to import & parse the bookmarks file. - currently, the bookmarks file name is unique to me, but will update this later.

---
+++ main.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+
+# Load and parse your bookmarks file.
+# This must be an HTML file! (HTML is the standard format when you export
+# from a web browser).
+with open('/mnt/data/bookmarks_7_27_25.html', 'r', encoding='utf-8') as file:
+    soup = BeautifulSoup(file, 'html.parser')
