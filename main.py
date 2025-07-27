6e6862d3637baa6df686568ffc7dbd88509b760a
Tyler <dialupdreams@proton.me>
2025-07-27 18:56:25 -0400

    Updated the rel. path for my bookmarks file location.

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from bs4 import BeautifulSoup
 # Load and parse your bookmarks file.
 # This must be an HTML file! (HTML is the standard format when you export
 # from a web browser).
-with open('/mnt/data/bookmarks_7_27_25.html', 'r', encoding='utf-8') as file:
+with open('~/Downloads/bookmarks_7_27_25.html', 'r', encoding='utf-8') as file:
     soup = BeautifulSoup(file, 'html.parser')
